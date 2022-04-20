57253c7a054a1c8f799ab3ff2808df93be84d482
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-04-19 23:23:47 -0400

    pushing the solution

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+//The 5th book in the series
+fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => console.log(`The 5th book in the series is ${json[4].name}`));
+//The 1031st character in the series
+fetch("https://anapioficeandfire.com/api/characters/1031")
+  .then((resp) => resp.json())
+  .then((json) => console.log(`The 1031st character in the GOT books is ${json.name}.`));
+//The total number of pages of all the books
+fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => json.map(book => book.numberOfPages))
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books').then(res => res.json()).then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
